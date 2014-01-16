6f43bd8e469f2af7d4dd42e90432c11636401c41
Tomás Martínez Arenas <matuser00@gmail.com>
2014-01-16 09:50:31 -0600

    Actualizacion index
    Actualizacion de imagenes, rutas y eso del index, todavia hay detalle
    con algunas imagenes

---
+++ TieneSentidoImpv1/application/views/bienvenido.php
@@ -0,0 +1,137 @@
+<?php include("header1.php");?>
+
+<div id="demo" class="skdslider">
+<ul>
+<li>
+    <img src="application/helpers/utilidades/skdslider/slides/1.jpg" />
+<!--Slider Description example-->
+ <div class="slide-desc">
+		<h2>Slider Title 1</h2>
+		<p>Demo description here. Demo description here. Demo description here. Demo description here. Demo description here. <a class="more" href="#">more</a></p>
+  </div>
+</li>
+<li><img src="application/helpers/utilidades/skdslider/slides/2.jpg" />
+   <div class="slide-desc">
+		<h2>Slider Title 2</h2>
+		<p>Demo description here. Demo description here. Demo description here. Demo description here. Demo description here. <a class="more" href="#">more</a></p>
+  </div>
+</li>
+<li><img src="application/helpers/utilidades/skdslider/slides/3.jpeg" />
+<!--NO Description Here-->
+</li>
+<li><img src="application/helpers/utilidades/skdslider/slides/4.jpg" />
+  <div class="slide-desc">
+		<h2>Slider Title 4</h2>
+		<p>Demo description here. Demo description here. Demo description here. Demo description here. Demo description here. <a class="more" href="#">more</a></p>
+  </div>
+</li>
+<li><img src="application/helpers/utilidades/skdslider/slides/5.jpg" /></li>
+<li><img src="application/helpers/utilidades/skdslider/slides/6.jpg" />
+  <div class="slide-desc">
+		<h2>Slider Title 6</h2>
+		<p>Demo description here. Demo description here. Demo description here. Demo description here. Demo description here. <a class="more" href="#">more</a></p>
+  </div>
+</li>
+</ul>
+</div>
+
+
+<div id="contenedor1" class="contenedor1" style="background: red;">
+    <article class="Artcontent">
+        <section id="contendorMenuIMG" class="contenedorARTmenu"> <a href="#"><img src="application/views/images/IMGquienes.png"/></a></section>
+         <section id="contendorMenuIMG" class="contenedorARTmenu"> <a href="#"><img src="application/views/images/IMGmision.png"/></a></section>
+         <section id="contendorMenuIMG" class="contenedorARTmenu"> <a href="#"><img src="application/views/images/IMGvision.png"/></a></section>
+         <section id="contendorMenuIMG" class="contenedorARTmenu"> <a href="#"><img src="application/views/images/IMGobjetivos.png"/></a></section>
+         <section id="contendorMenuIMG" class="contenedorARTmenu"> <a href="#"><img src="application/views/images/IMGcontacto.png"/></a></section>
+     </article>
+</div>
+
+
+
+<div id="contenedor1" class="contenedor1 main">
+    <article class="Artcontent">
+        <h2>Atenci&oacute;n</h2>
+        <p>Este sitio esta dirigido a personas Hipoacusticas o que no ecuchan, a las personas oyentes pedimos entiendan que el dise�o de esta aplicacion esta dirigido pensando en los compa�eros antes mecionados por lo tanto agradeceremos cualquier comentario, con la finalidad de mejorar la navegacion de nuestro sitio</p>
+            <h4>Gracias</h4>
+    </article>
+</div>
+
+<div id="demo1"  class="skdslider">
+<ul>
+<li>
+<img height="485" src="application/helpers/utilidades/skdslider/slides/1.jpg" />
+</li>
+<li><img height="485" src="application/helpers/utilidades/skdslider/slides/2.jpg" />
+</li>
+<li><img height="485" src="application/helpers/utilidades/skdslider/slides/3.jpeg" />
+<!--NO Description Here-->
+</li>
+<li><img height="485" src="application/helpers/utilidades/skdslider/slides/4.jpg" />
+</li>
+<li><img height="485" src="application/helpers/utilidades/skdslider/slides/5.jpg" /></li>
+<li><img height="485" src="application/helpers/utilidades/skdslider/slides/6.jpg" />
+
+</li>
+</ul>
+</div>
+
+
+<section id="section-04" >
+        <div class="content wide">
+          <div id="contenedor1" class="contenedor2 main">
+          <h2 class="ng-binding">Secciones de interes</h2>
+          <h3 class="ng-binding">en estas secciones podra encontrar promociones y noticas relacionadas con cultura y redes sociales.</h3>
+          </div>
+          <div class="columns">
+            <div class="col">
+              <h4 class="icon-check ng-binding">Noticias recientes</h4>
+              <p class="ng-binding">Los socios se actualizan sobre las �ltimas herramientas y productos de Google, y se presentan a ex�menes de certificaci�n administrados por Google cada 18 meses.</p>
+            </div>
+            <div class="col">
+              <h4 class="icon-check ng-binding">Redes sociales</h4>
+              <p class="ng-binding">El �xito comprobado es lo que destaca a los socios del resto. Utilizan las pr�cticas recomendadas de la industria para administrar campa�as y enfocarse en ayudarlo a aprovechar al m�ximo su presupuesto de marketing.</p>
+            </div>
+            <div class="col">
+              <h4 class="icon-check ng-binding">Anunciantes y promociones</h4>
+              <p class="ng-binding">Los socios se anticipan a todo, ya que trabajan directamente con Google. Cuentan con acceso a capacitaciones y notificaciones de productos que les permite estar siempre al tanto de lo que pasa en Google.</p>
+            </div>
+            
+          
+          <div class="col">
+              <h4 class="icon-check ng-binding">Videos</h4>
+              <p class="ng-binding">Los socios se anticipan a todo, ya que trabajan directamente con Google. Cuentan con acceso a capacitaciones y notificaciones de productos que les permite estar siempre al tanto de lo que pasa en Google.</p>
+          </div>
+            
+          </div>
+          
+        </div>
+      </section>
+
+
+<div id="contenedor1" class="contenedor1 ml160" style="width: 90%">
+    <article class="Artcontent">
+        <section id="contendorMenuIMG" class="contenedorARTmenu"> <a href="#"><img src="application/views/images/IMGnoticias.png"/></a></section>
+         <section id="contendorMenuIMG" class="contenedorARTmenu"> <a href="#"><img src="application/views/images/IMGpromociones.png"/></a></section>
+         <section id="contendorMenuIMG" class="contenedorARTmenu"> <a href="#"><img src="application/views/images/IMGPromociones.png"/></a></section>
+         <section id="contendorMenuIMG" class="contenedorARTmenu"> <a href="#"><img src="application/views/images/IMGvideos.png"/></a></section>
+        
+     </article>
+</div>
+
+
+<div id="demo1"  class="bannerFijo">
+    <a href="#"><img src="application/views/images/IMGslideFinal.png" width="1900"/></a>
+</div>
+
+
+<section id="section-04" >
+        <div class="content wide1">
+        <div id="contenedor1" class="contenedor2 main">
+          <h2 class="ng-binding">Donativos</h2>
+          <h3 class="ng-binding">Puedes hacer tus donativos, para apoyar este trabajo con las siguiente formas.</h3>
+        </div>
+     </div>
+</section>
+
+
+<?php include("footer1.php");?>
